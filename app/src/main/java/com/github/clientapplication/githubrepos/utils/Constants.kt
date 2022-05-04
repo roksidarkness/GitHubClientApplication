7a889a4dbc0462c6b526fff25d975e83e761d93b
roksidarkness <90980503+roksidarkness@users.noreply.github.com>
2022-05-04 12:10:11 +0300

    Add constants

--- app/src/main/java/com/github/clientapplication/githubrepos/utils/Constants.kt
+++ app/src/main/java/com/github/clientapplication/githubrepos/utils/Constants.kt
@@ -2,6 +2,8 @@ package com.github.clientapplication.githubrepos.utils
 
 object Constants {
 
+    const val TAG = "GITHUB_CLIENT"
+
     //Base urls
     const val DOMAIN_URL = "https://github.com/"
     const val GITHUB_GRAPHQL_API_URL = "https://api.github.com/graphql"
@@ -13,11 +15,13 @@ object Constants {
     const val PREFERENCE_NAME = "github_preference"
     const val PREF_KEY_TOKEN = "prefs_token"
 
-    const val TAG = "GITHUB_CLIENT"
-
     const val CLIENT_ID = "8a705a2f3b1e13fc6a03"
     const val CLIENT_SECRET = "c8ba35c5ff276f618c4fa2a4a9bcebb732531e47"
-    const val KEY_CODE= "code"
+    const val KEY_CODE = "code"
+    const val KEY_ERROR = "error"
+    const val PARAMETER_CLIENT_ID = "client_id"
+    const val PARAMETER_SCOPE = "scope"
+    const val VALUE_SCOPE = "repo"
 
 
 }
